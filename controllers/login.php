e4c6f9f831c0329835cf245e626e1cfe52db97d4
Georges <marhz>
2016-10-17 10:51:52 +0200

    v0.2

---
+++ controllers/login.php
@@ -0,0 +1,6 @@
+<?php
+
+	if(!isset($_SESSION['login']))
+	{
+		include('views/login.php');
+	}
--- layout.php
+++ layout.php
@@ -63,94 +63,13 @@
     </nav>
 
     <?php 
-        echo $content."<br>"; 
-        echo $_GET['p']."<br>";
-        echo $_SERVER['REQUEST_URI']."<br>";  
+        echo $content."<br>";
+
+        if(isset($_SESSION['login']))
+        {
+            include('views/plugins/sidebar.php');
+        } 
     ?>
-
-    <nav class="navbar navbar-default navbar-menuright col-lg-offset-9 col-lg-2">
-        <table class="table-condensed table-bordered table-striped">
-                <thead>
-                    <tr>
-                      <th colspan="7" >
-                        <a class="btn"><i class="icon-chevron-left"></i></a>
-                        <a class="btn">February 2012</a>
-                        <a class="btn"><i class="icon-chevron-right"></i></a>
-                      </th>
-                    </tr>
-                    <tr>
-                        <th>Su</th>
-                        <th>Mo</th>
-                        <th>Tu</th>
-                        <th>We</th>
-                        <th>Th</th>
-                        <th>Fr</th>
-                        <th>Sa</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td class="muted">29</td>
-                        <td class="muted">30</td>
-                        <td class="muted">31</td>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                    </tr>
-                    <tr>
-                        <td>5</td>
-                        <td>6</td>
-                        <td>7</td>
-                        <td>8</td>
-                        <td>9</td>
-                        <td>10</td>
-                        <td>11</td>
-                    </tr>
-                    <tr>
-                        <td>12</td>
-                        <td>13</td>
-                        <td>14</td>
-                        <td>15</td>
-                        <td>16</td>
-                        <td>17</td>
-                        <td>18</td>
-                    </tr>
-                    <tr>
-                        <td>19</td>
-                        <td><strong>20</strong></td>
-                        <td>21</td>
-                        <td>22</td>
-                        <td>23</td>
-                        <td>24</td>
-                        <td>25</td>
-                    </tr>
-                    <tr>
-                        <td>26</td>
-                        <td>27</td>
-                        <td>28</td>
-                        <td>29</td>
-                        <td class="muted">1</td>
-                        <td class="muted">2</td>
-                        <td class="muted">3</td>
-                    </tr>
-                </tbody>
-            </table>
-
-        <div class="moncompte">
-            <h1>Mon compte</h1>
-            <div>
-                <p>Bienvenue $Username</p>
-            </div>
-            <div>
-                <p>Il vous reste $Credit</p>
-            </div>
-            <h1>Mes formations</h1>
-            <div>
-                <p>Vous avez $nbrf le $fdate</p>
-            </div>
-        </div> 
-    </nav>
 
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
