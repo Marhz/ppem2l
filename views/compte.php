d76b70993c08112a857a3c9d21d482cd7a49f4e6
Tchoupie <gaetalim@gmail.com>
2016-10-17 11:25:51 +0200

    compte commit

---
+++ views/compte.php
@@ -0,0 +1,21 @@
+
+<div class="row">
+	<div class="col-md-4"></div>
+	<div class="col-md-3 navbar-default navbar">
+		<h2>Bonjour $username</h2>
+	</div>
+</div>
+<div class="row">
+	<nav class="navbar navbar-default">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+	      <a class="navbar-brand" href="#">Mon compte</a>
+	    </div>
+	    <ul class="nav navbar-nav">
+	      <li class="active"><a href="#">Information</a></li>
+	      <li><a href="#">Formation</a></li>
+	      <li><a href="#">Changement de mot de passe</a></li>
+	    </ul>
+	  </div>
+	</nav>
+</div>
