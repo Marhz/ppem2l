4e8dac34ff709b62dcef08670d9cc5d1b742b183
ThomasMignon <thomas.mignon51@laposte.com>
2017-02-21 14:50:53 +0100

    maj Welcome

--- controllers/welcome.php
+++ controllers/welcome.php
@@ -1,10 +1,13 @@
 <?php
 use Models\User;
+use Models\Formation;
+
 if(isset($_SESSION['connecte']))
 {
 	$user = auth('user');
 	//$nbFormation = $user->formations->count();
 	$nbFormations = $user->getNbFormations();
+	$formations = Formation::all()->take(5);
 	include_once('views/welcome.php');
 }
 else
--- views/welcome.php
+++ views/welcome.php
@@ -8,42 +8,22 @@
 						<th>Cout</th>
 						<th>Durée</th>
 						<th>Date de début</th>
-						<th>Date de fin</th>
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td>Mysql</td>
-						<td>ITIC</td>
-						<td>500 crédits</td>
-						<td>2 semaines</td>
-						<td>01/01/2016</td>
-						<td>15/01/2016</td>
-					</tr>
-					<tr>
-						<td>Mysql</td>
-						<td>ITIC</td>
-						<td>500 crédits</td>
-						<td>2 semaines</td>
-						<td>01/01/2016</td>
-						<td>15/01/2016</td>
-					</tr>
-					<tr>
-						<td>Mysql</td>
-						<td>ITIC</td>
-						<td>500 crédits</td>
-						<td>2 semaines</td>
-						<td>01/01/2016</td>
-						<td>15/01/2016</td>
-					</tr>
-					<tr>
-						<td>Mysql</td>
-						<td>ITIC</td>
-						<td>500 crédits</td>
-						<td>2 semaines</td>
-						<td>01/01/2016</td>
-						<td>15/01/2016</td>
-					</tr>
+					<?php 
+						foreach($formations as $formation)
+						{
+							echo "</tr>
+								<td>".$formation->titre."</td>
+								<td>ITIC</td>
+								<td>".$formation->cout."</td>
+								<td>".$formation->duree."</td>
+								<td>".$formation->debut."</td>
+							</tr>";
+						}
+					?>
+					
 				</tbody>
 			</table>
 		</div>
