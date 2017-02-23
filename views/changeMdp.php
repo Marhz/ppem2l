4ece7a369a7b391d2eb6a7443194a5e81c33e899
Tchoupie <gaetalim@gmail.com>
2017-02-23 17:59:33 +0100

    Changement vite fais
    18:00
    23/02/2017

--- views/changeMdp.php
+++ views/changeMdp.php
@@ -2,26 +2,26 @@
 <div class="container">
 	<form method="POST" action="changeMdp">
 		<div class="row">
-			<div class="form-group">
+			<div class="form-group col-md-push-3 col-md-6">
 				<label for="oldpassword">Mot de passe actuel : </label>
 				<input type="password" name="oldpassword" id="oldpassword" class="form-control " placeholder="" value="" required />
 			</div>
 		</div>
 		<div class="row">
-			<div class="form-group">
+			<div class="form-group col-md-push-3 col-md-6">
 				<label for="newpassword">Nouveau mot de passe :</label>
 				<input type="password" name="newpassword" id="newpassword" class="form-control " placeholder="" value="" required />
 			</div>
 		</div>
 		<div class="row">
-			<div class="form-group">
+			<div class="form-group col-md-push-3 col-md-6">
 				<label for="newpasswordconfirm">Confirmation du nouveau mot de passe :</label>
 				<input type="password" name="newpasswordconfirm" id="newpasswordconfirm" class="form-control " placeholder="" value="" required />
 			</div>
 		</div>
 		<div class="row">
-			<div class="form-group">
-				<input type="submit" name="submit" class="form-control" value="Valider"/>
+			<div class="form-group col-md-push-3 col-md-6">
+				<input type="submit" name="submit" class="form-control btn-primary" value="Valider"/>
 			</div>
 		</div>
 	</form>
