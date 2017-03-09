57e2c1f983e5ad93e9cfef49de8b7cbc9c499566
ThomasMignon <thomas.mignon51@laposte.com>
2017-03-09 16:31:58 +0100

    connection persistante && bdd change

--- controllers/login.php
+++ controllers/login.php
@@ -1,18 +1,48 @@
 <?php
 	if(isset($_SESSION['connecte']) || (!isset($_POST['email']) || !isset($_POST['mdp'])))
 	{
+		if(isset($_COOKIE['remember']))
+		{
+			$user = Models\User::where('token', $_COOKIE['remember'])->first();
+
+			if($user)
+			{			
+				$_SESSION['connecte']= true;
+				$_SESSION['user'] = $user;	
+				header('location:welcome');
+			}
+			else
+			{
+				$error = '<div class="alert alert-danger">Votre Cookie de connexion est expiré.</div>';
+				include_once('views/login.php');
+				//echo "Mauvais login <a href='index.php'>Retour</a>";
+			}
+
+		}
 		include('views/login.php');
 	}
 	else
-	{
+	{	
+		
+	
+
 		extract($_POST);
-		// include("models/login.php");
+		// include("models/login.php");	
+	
 		$user = Models\User::where('email', $email)->where('password', sha1($mdp))->first();
+		
+		
 		
 		if($user)
 		{			
 			$_SESSION['connecte']= true;
 			$_SESSION['user'] = $user;
+			if(isset($remember))
+            {
+            	$user->token = sha1(randStr(25));
+            	$user->save();
+                setcookie('remember',$user->token, time() + 365*24*3600);
+            }
 			echo "Connexion réussi";
 			header('location:welcome');
 		}
@@ -22,4 +52,5 @@
 			include_once('views/login.php');
 			//echo "Mauvais login <a href='index.php'>Retour</a>";
 		}
+		
 	}
--- controllers/logout.php
+++ controllers/logout.php
@@ -1,3 +1,10 @@
 <?php
+	if(isset($_COOKIE['remember']))
+	{
+		$user = Models\User::where('id', $_SESSION['user']->id)->first();
+		$user->token = "";
+		$user->save();
+		setcookie('remember',$user->token, time() - 42);
+	}
 	session_destroy();
 	header("location:index.php");
