bea2910462dacf26ec6bf88a73fe9231388a70e5
Gaetan <gaetalim@gmail.com>
2017-04-20 11:22:41 +0200

    Rajout du footer(non terminé)

---
+++ views/contact.php
@@ -0,0 +1,19 @@
+<div class="container">
+	<form method="POST" action="contact">
+		<div class="row">
+			<div class="form-group col-md-push-3 col-md-6">
+				<?= Core\Form::select("selectContact","Personne à contacter :",$optionsS) ?>
+			</div>
+		</div>
+		<div class="row">
+			<div class="form-group col-md-push-3 col-md-6">
+				<?= Core\Form::text("titreContact","Titre : ")?>
+			</div>
+		</div>
+		<div class="row">
+			<div class="form-group col-md-push-3 col-md-6">
+				<?= Core\Form::textarea("textContact","",$paramsT) ?> 
+			</div>
+		</div>
+	</form>
+</div>
